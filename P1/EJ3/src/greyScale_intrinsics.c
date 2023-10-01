ea5d5e97ee8784749b9783632d3ea35a36999fab
Monte-10 <61688940+Monte-10@users.noreply.github.com>
2023-10-01 13:14:27 +0200

    Cambios

--- P1/EJ3/src/greyScale_intrinsics.c
+++ P1/EJ3/src/greyScale_intrinsics.c
@@ -78,51 +78,44 @@ int main(int nargs, char **argv)
         }
 
         gettimeofday(&ini,NULL);
-        // RGB to grey scale
-        // int r, g, b;
-        // for (int i = 0; i < width; i++)
-        // {
-        //     for (int j = 0; j < height; j++)
-        //     {
-        //         getRGB(rgb_image, width, height, 4, i, j, &r, &g, &b);
-        //         grey_image[j * width + i] = (int)(0.2989 * r + 0.5870 * g + 0.1140 * b);
-        //     }
-        // }
 
-    	// Solución
     	for (int j=0; j<height; j++) {
     		for (int i=0; i<width; i+=4) { // 4 pixeles / bucle
 
-                // printf("Pixel: [%d x %d]\n", i,j);
-
-                int image_offset = (i + j*width)*4; // 4 bytes por pixel
+                // 4 bytes por pixel
+                int image_offset = (i + j*width)*4; 
     			uint8_t* image = rgb_image + image_offset;
 
                 // _mm_loadu_si128: Load 128-bits of integer data from memory into dst. mem_addr does not need to be aligned on any particular boundary.
 
                 // 4 pixeles en 2 vectores de 2 pixeles cada uno (8 bytes por vector)
                 // double = 64 bits = 8 bytes
-                __m128i datal = _mm_loadu_si64(image);      // Data Low: 2 pixels = 8 bytes = 64 bits
-                __m128i datah = _mm_loadu_si64(image+8);    // Data High: 2 pixels = 8 bytes = 64 bits
+                // Data Low: 2 pixels = 8 bytes = 64 bits
+                __m128i datal = _mm_loadu_si64(image);      
+                // Data High: 2 pixels = 8 bytes = 64 bits
+                __m128i datah = _mm_loadu_si64(image+8);    
 
                 // Extender a vectores de 126 bits (enteros de 16 bits)
                 // Low: vec_a, vec_b
                 // High: vec_c, vec_d
                 // _mm256_cvtepu8_epi32: Zero extend packed unsigned 8-bit integers in a to packed 32-bit integers, and store the results in dst.
-
-                __m256i elow = _mm256_cvtepu8_epi32(datal); // Extended Low
-                __m256i ehigh = _mm256_cvtepu8_epi32(datah); // Extended High
+                // Extended Low
+                __m256i elow = _mm256_cvtepu8_epi32(datal); 
+                // Extended High
+                __m256i ehigh = _mm256_cvtepu8_epi32(datah); 
 
                 // int to float
                 // _mm256_cvtepi32_ps: Convert packed signed 32-bit integers in a to packed single-precision (32-bit) floating-point elements, and store the results in dst.
-
-                __m256 flow = _mm256_cvtepi32_ps(elow); // Floating Low
-                __m256 fhigh = _mm256_cvtepi32_ps(ehigh); // Floating High
+                // Floating Low
+                __m256 flow = _mm256_cvtepi32_ps(elow); 
+                // Floating High
+                __m256 fhigh = _mm256_cvtepi32_ps(ehigh); 
 
                 // Coefficients
                 // _mm256_set_pd: Set packed double-precision (64-bit) floating-point elements in dst with the supplied values.
                 // __m256 holds a pair of coefficients for each of the two pixels in low and high vectors
-                __m256 coefficients = _mm256_set_ps(0, 0.1140, 0.5870, 0.2989, 0, 0.1140, 0.5870, 0.2989); // Twice for two pixels
+                // Twice for two pixels
+                __m256 coefficients = _mm256_setr_ps(0.2989, 0.5870, 0.114, 0, 0.2989, 0.5870, 0.114, 0); 
 
                 // Multiplicar pixeles por coeficientes
                 flow = _mm256_mul_ps(flow, coefficients);
@@ -132,22 +125,24 @@ int main(int nargs, char **argv)
                 // fvec_abcd <- fvec_ab hadd fvec_cd
                 // result <- fvec_abcd hadd fvec_abcd
                 // _mm256_hadd_ps: Horizontally add adjacent pairs of single-precision (32-bit) floating-point elements in a and b, and pack the results in dst.
-                __m256 result = _mm256_hadd_ps(flow, fhigh);
-                result = _mm256_hadd_ps(result, result);
+                __m256 sumpartial = _mm256_hadd_ps(flow, fhigh);
+                __m256 sumfinal = _mm256_floor_ps(_mm256_hadd_ps(sumpartial, sumpartial));
 
                 // Reorder values from vector: [ACACBDBD]
                 // _mm256_permutevar8x32_ps
                 // 0, 4, 1, 5, 2, 6, 3, 7 (reverse)
-                __m256i permute_offsets = _mm256_set_epi32(7,3,6,2,5,1,4,0); // transforms [ACACBDBD] into [ABCDABCD]
-                result = _mm256_permutevar8x32_ps(result, permute_offsets);
+                __m256i vperm = _mm256_setr_epi32(0, 4, 1, 5, 0, 0, 0, 0); 
+                // transforms [ACACBDBD] into [ABCDABCD]
+                __m256 resultpartial = _mm256_permutevar8x32_ps(sumfinal, vperm);
 
                 // extract 4 pixels
-                __m128i res = _mm_cvtps_epi32(_mm256_extractf128_ps(result, 0));
+                __m128i result = _mm_cvtps_epi32(_mm256_extractf128_ps(resultpartial, 0));
 
                 // Store result
-                uint8_t* greyDest = grey_image + image_offset/4;
-                _mm_storeu_si128(((__m128i*) greyDest), res);
-
+                __m128i perm = _mm_setr_epi8(0, 1*4, 2*4, 3*4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                result = _mm_shuffle_epi8(result, perm);
+                uint32_t *tt = (uint32_t*)&result;
+                *((uint32_t*)(&grey_image[i + j*width])) = *tt;
     		}
     	}
 
--- P1/simple2.c
+++ P1/simple2.c
@@ -1,32 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <x86intrin.h>
+#include <immintrin.h>
+#include <sys/time.h>
 
 #define ARRAY_SIZE 2048
-#define NUMBER_OF_TRIALS 1000000
+#define NUMBER_OF_TRIALS 100000
 
-/*
- * Statically allocate our arrays.  Compilers can
- * align them correctly.
- */
+
 static double a[ARRAY_SIZE], b[ARRAY_SIZE], c;
 
+
 int main(int argc, char *argv[]) {
-    int i,t;
+    int i, t;
+    int n_trials;
 
-    double m = 1.0001;
+    struct timeval start, finish;
 
-    /* Populate A and B arrays */
-    for (i=0; i < ARRAY_SIZE; i++) {
-        b[i] = i;
-        a[i] = i+1;
+    __m256d mm = {1.0001, 1.0001, 1.0001, 1.0001}; // Crea un vector con 4 copias del valor m = 1.0001
+    __m256d sum = {0.0, 0.0, 0.0, 0.0}; // to hold partial sums
+
+    __m256d aux_b = {0,1,2,3};
+    __m256d aux_a = {1,2,3,4};
+    __m256d cuatros = {4.0,4.0,4.0,4.0};
+
+    // Poblamos los arrays A y B
+    for (i=0; i < ARRAY_SIZE; i+=4) {
+            _mm256_store_pd(&b[i], aux_b); 
+            _mm256_store_pd(&a[i], aux_a); 
+
+            aux_b = _mm256_hadd_pd(aux_b, cuatros);
+            aux_a = _mm256_hadd_pd(aux_a, cuatros); 
     }
 
-    /* Perform an operation a number of times */
-    for (t=0; t < NUMBER_OF_TRIALS; t++) {
-        for (i=0; i < ARRAY_SIZE; i++) {
-            c += m*a[i] + b[i];
+    for(n_trials = 100000; n_trials < 1000000; n_trials += 100000){
+        c = 0.0;
+        __m256d sum = {0.0, 0.0, 0.0, 0.0};
+
+        gettimeofday(&start, NULL);
+
+        for (t = 0; t < n_trials; t++) {
+            for (i = 0; i < ARRAY_SIZE; i += 4) {
+                __m256d vec_a = _mm256_loadu_pd(&a[i]);
+                __m256d vec_b = _mm256_loadu_pd(&b[i]);
+
+
+                __m256d tmp = _mm256_fmadd_pd (mm, vec_a , vec_b);
+                sum = _mm256_add_pd (tmp, sum);
+            }
         }
+
+        for (i = 0; i < 4; i++) {
+            c += sum[i];
+        }
+
+        gettimeofday(&finish, NULL);
+
+        printf("Time: %f\n", (double) (finish.tv_sec - start.tv_sec) + (finish.tv_usec - start.tv_usec) / 1000000.0);
     }
 
-    return 0;
+
+
+   return 0;
 }
